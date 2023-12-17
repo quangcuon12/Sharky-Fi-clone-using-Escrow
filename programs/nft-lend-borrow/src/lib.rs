9caf5ddcff17b66f571a01898b197419fa42a625
quangcuon12 <154036260+quangcuon12@users.noreply.github.com>
2023-12-17 15:08:45 +0700

    declare lending program id

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("G4bFtvMpc4Se9j2WvquCEkhg2pwaF692vvsSJ1Y3jB5b");
 
 #[program]
 pub mod nft_lend_borrow {
